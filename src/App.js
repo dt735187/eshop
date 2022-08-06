5723672d07366a39977117ea337303f6ad6f1223
dt735187 <dipakkumart584@gmail.com>
2022-08-06 14:22:25 +0530

    login regiister reset jsx  completed

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 
 import { Header,Footer } from './components'
 
-import { Home,Contact } from './pages'
+import { Home,Contact, Login, Register, Reset } from './pages'
 
 const App = () => {
   return (
     <>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-      <Footer />
-    </Router>
-    
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
 export default App

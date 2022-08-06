ddb35178b4e7699f8c61f4bd27e35fbe9838e3d9
dt735187 <dipakkumart584@gmail.com>
2022-08-06 14:05:54 +0530

    footer section completed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import styles from "./footer.module.scss";
+
+const date = new Date();
+const year = date.getFullYear();
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return <div className={styles.footer}>&copy; {year} All Rights Reserved</div>;
+};
 
-export default Footer
+export default Footer;
